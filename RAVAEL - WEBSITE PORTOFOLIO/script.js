d52485bafe4c2cee98ca7b8917dc75e929b5b6eb
ravaelchessario24 <ravaelchessario@gmail.com>
2025-02-05 21:41:52 +0700

    Add files via upload

---
+++ RAVAEL - WEBSITE PORTOFOLIO/script.js
@@ -0,0 +1,53 @@
+let nav = document.querySelector("nav");
+let scrollBtn = document.querySelector(".scroll-button a");
+// Show/hide sticky navigation and scroll button based on scroll position
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 20) {
+    nav.classList.add("sticky");
+    scrollBtn.style.display = "block";
+  } else {
+    nav.classList.remove("sticky");
+    scrollBtn.style.display = "none";
+  }
+};
+// Side Navigation Menu
+let body = document.querySelector("body");
+let navBar = document.querySelector(".navbar");
+let menuBtn = document.querySelector(".menu-btn");
+let cancelBtn = document.querySelector(".cancel-btn");
+// Open side navigation
+menuBtn.onclick = function () {
+  navBar.classList.add("active");
+  menuBtn.style.opacity = "0";
+  menuBtn.style.pointerEvents = "none";
+  body.style.overflow = "hidden";
+  scrollBtn.style.pointerEvents = "none";
+};
+const hideNavMenu = () => {
+  navBar.classList.remove("active");
+  menuBtn.style.opacity = "1";
+  menuBtn.style.pointerEvents = "auto";
+  body.style.overflow = "auto";
+  scrollBtn.style.pointerEvents = "auto";
+};
+function updateLayout() {
+  const container = document.querySelector(".projects-container");
+
+  if (window.innerWidth <= 768) {
+    container.classList.add("mobile-view");
+    container.classList.remove("desktop-view");
+  } else {
+    container.classList.add("desktop-view");
+    container.classList.remove("mobile-view");
+  }
+}
+
+window.addEventListener("resize", updateLayout);
+window.addEventListener("load", updateLayout);
+// Close side navigation
+cancelBtn.onclick = hideNavMenu;
+// Close side navigation when a menu link is clicked
+let navLinks = document.querySelectorAll(".menu li a");
+navLinks.forEach((link) => {
+  link.addEventListener("click", hideNavMenu);
+});
